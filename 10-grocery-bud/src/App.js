33098047a466bc0f3a60d26808dfdfd017cc4fd2
munish710 <munish710@gmail.com>
2021-07-06 19:08:18 +0530

    added List COmponent and updated List display logic

--- 10-grocery-bud/src/App.js
+++ 10-grocery-bud/src/App.js
@@ -11,7 +11,15 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("hello");
+    if (!name) {
+      //display alert
+    } else if (name && isEditing) {
+      //Edit Case
+    } else {
+      //show alert
+      const newItem = { id: new Date().getTime().toString(), title: name };
+      setList([...list, newItem]);
+    }
   };
   return (
     <section className="section-center">
@@ -31,10 +39,12 @@ function App() {
           </button>
         </div>
       </form>
-      <div className="grocery-container">
-        <List />
-        <button className="clear-btn">Clear Items</button>
-      </div>
+      {list.length > 0 && (
+        <div className="grocery-container">
+          <List items={list} />
+          <button className="clear-btn">Clear Items</button>
+        </div>
+      )}
     </section>
   );
 }
