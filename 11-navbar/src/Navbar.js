229a492cbe09e7b2a6d86832680b320140cd34c2
munish710 <munish710@gmail.com>
2021-07-07 00:46:48 +0530

    Added navbar component

--- 11-navbar/src/Navbar.js
+++ 11-navbar/src/Navbar.js
@@ -4,7 +4,56 @@ import { links, social } from "./data";
 import logo from "./logo.svg";
 
 const Navbar = () => {
-  return <h4>navbar</h4>;
+  return (
+    <nav>
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} alt="logo" />
+          <button className="nav-toggle">
+            <FaBars />
+          </button>
+        </div>
+        <div className="links-container show-container">
+          <ul className="links">
+            <li>
+              <a href="#">home</a>
+            </li>
+            <li>
+              <a href="#">about</a>
+            </li>
+            <li>
+              <a href="#">contact</a>
+            </li>
+            <li>
+              <a href="#">products</a>
+            </li>
+          </ul>
+        </div>
+        <ul className="social-icons">
+          <li>
+            <a href="hhtps://twitter.com">
+              <FaTwitter />
+            </a>
+          </li>
+          <li>
+            <a href="hhtps://twitter.com">
+              <FaTwitter />
+            </a>
+          </li>
+          <li>
+            <a href="hhtps://twitter.com">
+              <FaTwitter />
+            </a>
+          </li>
+          <li>
+            <a href="hhtps://twitter.com">
+              <FaTwitter />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
