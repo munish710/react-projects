93419518538cd3bafde910d9ae612f9b2d444d04
munish710 <munish710@gmail.com>
2021-07-07 23:41:41 +0530

    added modal and sidebar component

--- 12-sidebar-modal/src/Modal.js
+++ 12-sidebar-modal/src/Modal.js
@@ -1,7 +1,16 @@
 import React from "react";
 import { FaTimes } from "react-icons/fa";
 const Modal = () => {
-  return <h2>Modal</h2>;
+  return (
+    <div className={`modal-overlay `}>
+      <div className="modal-container">
+        <h3>modal content</h3>
+        <buttton className="close-modal-btn">
+          <FaTimes />
+        </buttton>
+      </div>
+    </div>
+  );
 };
 
 export default Modal;
