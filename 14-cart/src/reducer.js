cf5c04103ee29cbc7da38a79cf510c34f0290686
munish710 <munish710@gmail.com>
2021-07-09 00:03:49 +0530

    added increase decrease function  for individual cartItem

--- 14-cart/src/reducer.js
+++ 14-cart/src/reducer.js
@@ -2,12 +2,28 @@ const reducer = (state, action) => {
   switch (action.type) {
     case "CLEAR_CART":
       return { ...state, cart: [] };
-      break;
     case "REMOVE":
       return {
         ...state,
         cart: state.cart.filter((cartItem) => cartItem.id !== action.payload),
       };
+    case "INCREASE":
+      let tempCart = state.cart.map((cartItem) => {
+        if (cartItem.id === action.payload) {
+          return { ...cartItem, amount: cartItem.amount + 1 };
+        }
+        return cartItem;
+      });
+      return { ...state, cart: tempCart };
+    case "DECREASE":
+      let tempCart2 = state.cart.map((cartItem) => {
+        if (cartItem.id === action.payload) {
+          return { ...cartItem, amount: cartItem.amount - 1 };
+        }
+        return cartItem;
+      });
+      return { ...state, cart: tempCart2 };
+
     default:
       return state;
   }
