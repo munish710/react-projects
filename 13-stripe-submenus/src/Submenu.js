c789e67497f7562404d379236df09b92b6aaa5ef
munish710 <munish710@gmail.com>
2021-07-08 17:17:17 +0530

    added submenu component

--- 13-stripe-submenus/src/Submenu.js
+++ 13-stripe-submenus/src/Submenu.js
@@ -1,6 +1,13 @@
 import React, { useState, useRef, useEffect } from "react";
+import { useGlobalContext } from "./context";
 
 const Submenu = () => {
+  const { isSubmenuOpen } = useGlobalContext();
+  return (
+    <aside className={`${isSubmenuOpen ? "submenu show" : "submenu"}`}>
+      submenu
+    </aside>
+  );
   return <h2>submenu component</h2>;
 };
 
